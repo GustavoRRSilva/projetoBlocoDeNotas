49249d9e839d14b7ba2a6f23247942107def99c6
Gustavo <gstvrrds@gmail.com>
2023-08-22 08:49:30 -0300

    trocando a orientação da tela com base no tamanho da tela do usuario

--- projetoBlocoDeNotas/Front-End/src/App.js
+++ projetoBlocoDeNotas/Front-End/src/App.js
@@ -1,6 +1,18 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Background from "./componentes/BackgroundPrincipal/index.js";
 function App(props) {
+const [tamanhoTela,setTamanhoTela] = useState("");
+
+useEffect(() =>{
+  const trocaTamanho = ()=>{
+    setTamanhoTela(window.innerWidth)
+  }
+  window.addEventListener("resize",trocaTamanho);
+  return()=>{
+    window.removeEventListener("resize",trocaTamanho)
+  }
+})
 
   if(window.innerWidth >= 776){
     return (
