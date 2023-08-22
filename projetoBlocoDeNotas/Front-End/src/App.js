d8d17cdb4678823c0edf80f5e86a78a573aa513b
Gustavo <gstvrrds@gmail.com>
2023-08-21 21:21:14 -0300

    invertendo background com base no tamanho da tela

--- projetoBlocoDeNotas/Front-End/src/App.js
+++ projetoBlocoDeNotas/Front-End/src/App.js
@@ -1,22 +1,44 @@
 import "./App.css";
 import Background from "./componentes/BackgroundPrincipal/index.js";
 function App(props) {
-  return (
+
+  if(window.innerWidth >= 776){
+    return (
+      <div className="backgroundGeral">
+        <Background
+          color="#6F7357"
+          colorLado="#FFFFFF"
+          alinhamento="rigth"
+          padding="0 0 0 20px"
+        ></Background>
+        <Background
+          color="#273A2D"
+          colorLado="#6F7357"
+          alinhamento="left"
+          padding = '0 20px 0 0'
+        ></Background>
+      </div>
+    );
+  }
+  else{
+    return (
     <div className="backgroundGeral">
       <Background
         color="#6F7357"
         colorLado="#FFFFFF"
-        alinhamento="rigth"
-        padding="0 0 0 20px"
+        alinhamento="end"
+        padding=" 0 20px"
       ></Background>
       <Background
         color="#273A2D"
         colorLado="#6F7357"
-        alinhamento="left"
-        padding="0 20px 0 0"
+        alinhamento="start"
+        padding="0 20px"
       ></Background>
     </div>
   );
+  }
+  
 }
 
 export default App;
--- projetoBlocoDeNotas/Front-End/src/componentes/BackgroundPrincipal/index.js
+++ projetoBlocoDeNotas/Front-End/src/componentes/BackgroundPrincipal/index.js
@@ -2,18 +2,35 @@ import React from "react";
 import LadoLogin from "../LadoLogin/index.js";
 import "./BackgroundPrincipal.css";
 function Background(props) {
-  return (
-    <div
-      className="background"
-      style={{
-        backgroundColor: props.color,
-        justifyContent: props.alinhamento,
-        padding: props.padding,
-      }}
-    >
-      <LadoLogin colorLado={props.colorLado}></LadoLogin>
-    </div>
-  );
+  if(window.innerHeight >= 776){
+    return (
+      <div
+        className="background"
+        style={{
+          backgroundColor: props.color,
+          justifyContent: props.alinhamento,
+          padding: props.padding,
+        }}
+      >
+        <LadoLogin colorLado={props.colorLado}></LadoLogin>
+      </div>
+    );
+  }
+  else{
+    return (
+      <div
+        className="background"
+        style={{
+          backgroundColor: props.color,
+          alignItems: props.alinhamento,
+          padding: props.padding,
+        }}
+      >
+        <LadoLogin colorLado={props.colorLado}></LadoLogin>
+      </div>
+    );
+  }
+ 
 }
 
 export default Background;
